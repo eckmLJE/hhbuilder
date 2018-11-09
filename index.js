a08fc3760840e82cd905eb1b6da97e81bcb6cd5e
eckmLJE <lucas.eckman@gmail.com>
2018-11-09 08:32:54 -0600

    selectors

--- index.js
+++ index.js
@@ -4,4 +4,35 @@
 // Display the household list in the HTML as it is modified
 // Serialize the household as JSON upon form submission as a fake trip to the server
 
-console.log("index loaded");
+// console.log("index loaded");
+
+var store = {
+  personBuilder: {
+    relationship: "",
+    age: null,
+    smoker: false
+  },
+  household: []
+};
+
+// SELECTORS
+
+function getAge() {
+  return document.querySelector('input[name="age"]');
+}
+
+function getRelationship() {
+  return document.querySelector('select[name="rel"]');
+}
+
+function getSmoker() {
+  return document.querySelector('input[name="smoker"]');
+}
+
+function getAdd() {
+  return document.querySelector(".add");
+}
+
+function getSubmit() {
+  return document.querySelector('button[type="submit"');
+}
